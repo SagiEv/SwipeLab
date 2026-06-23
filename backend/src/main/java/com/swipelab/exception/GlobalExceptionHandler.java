63df2302c860ade3225d1623ba14f8200d2b8ca5
Eden Bar <barede@post.bgu.ac.il>
2026-06-23 22:04:27 +0300

    Fix missing @Slf4j in GlobalExceptionHandler

--- backend/src/main/java/com/swipelab/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/swipelab/exception/GlobalExceptionHandler.java
@@ -8,11 +8,13 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
